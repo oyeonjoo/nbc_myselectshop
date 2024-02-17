9d7dde057847e8b2987f5e6ee4b38e22cb365124
oyeonjoo <oyj0842@naver.com>
2024-02-18 01:14:29 +0900

    rename

--- src/main/java/com/sparta/myselectshop/controller/HomeController.java
+++ src/main/java/com/sparta/myselectshop/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.sparta.myselectshop.naver.controller;
+package com.sparta.myselectshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
