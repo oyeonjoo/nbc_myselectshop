5881531cde18c1af4be92af2445024dd8ddedcc0
oyeonjoo <oyj0842@naver.com>
2024-02-20 00:49:50 +0900

    폴더 별 관심상품 조회 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -11,4 +11,20 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByUser(User user, Pageable pageable);
 
     Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
+    // select * from product p
+    // where p.user_id = 1;
+
+    // select
+    //   p.id,
+    //   p.title as product_title,
+    //   pf.product_id as product_id,
+    //   pf.folder_id as folder_id
+    // from
+    //      product p left join product_folder pf
+    //          on p.id = pf.product_id
+    // where p.user_id = 1
+    //    and
+    //    pf.folder_id = 3
+    // order by p.id desc
+    //      limit 10, 10;      -> 갯수 제한, 페이징
 }
